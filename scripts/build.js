e5538c3d22b04f826b86793ddbca8911442683c1
Brian Belcher <belcher.bn@gmail.com>
2022-07-08 13:01:38 -0400

    update build

--- scripts/build.js
+++ scripts/build.js
@@ -7,18 +7,20 @@ const defaults = rewire('react-scripts/scripts/build.js');
 // Getting configuration from original build script
 let config = defaults.__get__('config');
 
-// If we want to move build result into a different folder, we can do that!
-// Please note: that should be an absolute path!
-//config.output.path = path.join(path.dirname(__dirname), 'build/js');
+// Move build result into a different folder. (should be absolute path)
+//config.output.path = path.join(path.dirname(__dirname), 'build-github');
 
-// If we want to rename resulting bundle file to not have hashes, we can do that!
+// Rename bundle path and filename, to what CSF site expects
 config.output.filename = 'js/toolinit.js';
 
-// do same for css
+// Rename css path and filename, to what CSF load-dependencies.js expects.
+// 'load-dependencies.js' is placed in public/js, and loaded by CSF site.
+// It loads this specified css file, so path/filename needs to be correct.
 const miniCssExtractPlugin = config.plugins.find(element => element.constructor.name === "MiniCssExtractPlugin");
 miniCssExtractPlugin.options.filename = "style/csf-gddtool-v4.css"
 
-// And the last thing: disabling splitting
+// Disable splitting
+// We need single files for loading in CSF, must avoid chunking files.
 config.optimization.splitChunks = {
     cacheGroups: {
         default: false,
